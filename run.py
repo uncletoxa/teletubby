5c2a96acd197698aacc21ededf0e30f9737e9073
uncletoxa <7903257+uncletoxa@users.noreply.github.com>
2024-04-07 19:38:39 +0200

    rewrite to pyrogram to bypass limitation of 20mb per file

--- run.py
+++ run.py
@@ -3,12 +3,29 @@ import os
 import sqlite3
 import argparse
 import yt_dlp
-from subprocess import run, PIPE
+from pyrogram import Client
+from decouple import config
+import uvloop
+
 
 DB_NAME = 'videos.db'
 
+API_ID = config('API_ID', cast=int)
+API_HASH = config('API_HASH')
+BOT_TOKEN = config('BOT_TOKEN')
 
-def fetch_and_notify(channel_id, folder_path, dry_run):
+
+def progress(current, total, video_id):
+    logging.info(f"loading {video_id}: {current * 100 / total:.1f}%")
+
+
+def record_video_to_db(conn, id, title):
+    conn.execute("INSERT INTO videos(video_id, title) VALUES(?, ?)",
+                 (id, title))
+    logging.info(f'Video {id}, {title} has been added')
+
+
+def fetch_and_notify(app, yt_channel_id, tg_chat_id, folder_path, dry_run):
     ydl_opts = {'quiet': True, 'extract_flat': True, 'outtmpl': f'{folder_path}/%(id)s.%(ext)s', 'format': 'mp4'}
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -20,7 +37,7 @@ def fetch_and_notify(channel_id, folder_path, dry_run):
                     video_id TEXT PRIMARY KEY,
                     title TEXT); """)
 
-            result = ydl.extract_info(f'https://www.youtube.com/{channel_id}/videos', download=False)
+            result = ydl.extract_info(f'https://www.youtube.com/{yt_channel_id}/videos', download=False)
 
             if 'entries' in result:
                 for video in result['entries']:
@@ -42,35 +59,47 @@ def fetch_and_notify(channel_id, folder_path, dry_run):
                                 video_title_fmt = f"<b>{video['title']}</b>"
                                 video_description_fmt = f"\n{video['description']}" if video['description'] else ''
 
-                                response = run([
-                                        "telegram-send",
-                                        "--video", file_path,
-                                        "--caption", video_title_fmt + video_description_fmt,
-                                        "--format", "html",
-                                        "--showids"],
-                                    text=True, stdout=PIPE, stderr=PIPE)
+                                logging.info(f'Sending {video_id} to {tg_chat_id}')
+                                app.send_video(
+                                    chat_id=tg_chat_id,
+                                    video=file_path,
+                                    caption=video_title_fmt + video_description_fmt,
+                                    progress=progress,
+                                    progress_args=(video_id,)
+                                )
+                                logging.info(f'Video {video_id} has been sent')
+                                record_video_to_db(conn, video_id, video_title)
 
-                                if response.returncode != 0:
-                                    raise RuntimeError(
-                                        f'Video has not been send:\n{response.stdout}')
+                            except Exception as e:
+                                logging.error(f'Error occurred while sending video {video_id}: {str(e)}')
+
                             finally:
                                 if os.path.exists(file_path):
                                     os.remove(file_path)
                                     logging.info(f'Video file {file_path} has been removed')
+                        else:
+                            record_video_to_db(conn, video_id, video_title)
 
-                            conn.execute("INSERT INTO videos(video_id, title) VALUES(?, ?)", (video_id, video_title))
-                            logging.info(f'Video {video_id}, {video_title} has been added')
 
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
+def main(app):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--channel-id', required=True)
+    parser.add_argument('-y', '--yt-channel-id', required=True)
+    parser.add_argument('-t', '--tg-chat-id', required=True)
     parser.add_argument('-d', '--dry-run', action='store_true')
+    parser.add_argument('-l', '--log-level', default='info')
     parser.add_argument('-f', '--folder', default='/tmp', help='Folder path to save videos (default: /tmp)')
 
     args = parser.parse_args()
+    numeric_level = getattr(logging, args.log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.log_level)
+    logging.basicConfig(level=numeric_level, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    fetch_and_notify(args.channel_id, args.folder, args.dry_run)
+    fetch_and_notify(app, args.yt_channel_id, args.tg_chat_id, args.folder, args.dry_run)
     logging.info(f"Job finished")
+
+
+if __name__ == '__main__':
+    uvloop.install()
+    with Client("teletubby", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN) as app:
+        main(app)
